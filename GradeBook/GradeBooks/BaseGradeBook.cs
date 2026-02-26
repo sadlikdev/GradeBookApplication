740135ed61b40f159d778bf31434b9f0663c058e
Patrycja Sadlik <patrycja.sadlik@microsoft.wsei.edu.pl>
2026-02-26 23:48:52 +0100

    UpdatedHelpCommandText&AddedExtraGPA

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -108,20 +108,29 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var gpa = 0d;
+
             switch (letterGrade)
             {
-                case 'A':
-                    return 4;
-                case 'B':
-                    return 3;
-                case 'C':
-                    return 2;
-                case 'D':
-                    return 1;
-                case 'F':
-                    return 0;
+               case 'A':
+                    gpa = 4;
+                    break;
+               case 'B':
+                    gpa = 3;
+                    break;
+               case 'C':
+                    gpa = 2;
+                    break;
+               case 'D':
+                    gpa = 1;
+                    break;
+               case 'F':
+                    gpa = 0;
+                    break;
             }
-            return 0;
+            if (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled)   
+                gpa += 1;
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
